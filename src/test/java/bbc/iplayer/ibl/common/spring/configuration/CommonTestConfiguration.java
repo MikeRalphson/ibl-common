eb876a312eb6a3c374469d0727ae69df43ee3a70
Stuart Rivero <stuart.rivero.ext@bbc.co.uk>
2013-10-07 09:36:12 +0100

    log4j

--- src/test/java/bbc/iplayer/ibl/common/spring/configuration/CommonTestConfiguration.java
+++ src/test/java/bbc/iplayer/ibl/common/spring/configuration/CommonTestConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(value = CommonConfiguration.class)
+@Import({CommonConfiguration.class})
 public class CommonTestConfiguration {
-
 }
