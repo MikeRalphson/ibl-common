424a98f0bd68ea2360b95f73a5a139a4491f66e0
Stuart Rivero <stuart.rivero.ext@bbc.co.uk>
2013-10-25 15:58:52 +0100

    Added test for broadcast info

--- src/main/java/bbc/iplayer/ibl/common/concurrency/FutureCallbackAdapter.java
+++ src/main/java/bbc/iplayer/ibl/common/concurrency/FutureCallbackAdapter.java
@@ -8,19 +8,17 @@ public class FutureCallbackAdapter<T> implements FutureCallback<T> {
     private static final Logger LOG = LoggerFactory.getLogger(FutureCallbackAdapter.class);
     private final String info;
 
-    public FutureCallbackAdapter(String info){
+    public FutureCallbackAdapter(String info) {
         this.info = info;
     }
 
-
-
     @Override
     public void onSuccess(T obj) {
-        LOG.debug("Success:{}",info);
+        LOG.debug("Success:{}", info);
     }
 
     @Override
     public void onFailure(Throwable throwable) {
-        LOG.error("Failed to get: "+info, throwable);
+        LOG.error("Failed to get: " + info, throwable);
     }
 }
