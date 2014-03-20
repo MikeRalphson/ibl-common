5dea3fca0ca12c40bc295cae04f8fd65ae3c8b76
nick spragg <nick.spragg@bbc.co.uk>
2014-03-20 08:52:30 +0000

    place holder

---
+++ src/main/java/uk/co/bbc/iplayer/common/concurrency/MoreFuturesDSL.java
@@ -0,0 +1,16 @@
+package uk.co.bbc.iplayer.common.concurrency;
+
+import com.google.common.util.concurrent.Futures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class MoreFuturesDSL {
+
+    List<?> futures = new ArrayList();
+
+    public void test() {
+
+    }
+
+}
