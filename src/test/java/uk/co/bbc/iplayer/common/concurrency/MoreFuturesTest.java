e776b5ce826754d5721c78025cd9c39e5c7f9b71
nick spragg <nick.spragg@bbc.co.uk>
2014-03-20 10:05:33 +0000

    Fixed after merge with master

--- src/test/java/uk/co/bbc/iplayer/common/concurrency/MoreFuturesTest.java
+++ src/test/java/uk/co/bbc/iplayer/common/concurrency/MoreFuturesTest.java
@@ -4,16 +4,11 @@ import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.rules.ExpectedException;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.mockito.Matchers.any;
@@ -23,69 +18,104 @@ import static org.mockito.Mockito.*;
 public class MoreFuturesTest {
 
     private static ListeningExecutorService executorService;
-    private static Callable<Void> taskToComplete;
-    private static Callable<Void> futureThatTakesTooLong;
-    private static Callable<Void> taskThrowsACheckedException;
-    private static Callable<Void> taskThrowsAnUncheckedException;
-    private static Callable<Void> taskToCompleteImmediately;
+    private static Callable<String> taskToComplete;
+    private static Callable<String> futureThatTakesTooLong;
+    private static Callable<String> taskThrowsARuntimeException;
+    private static Callable<String> taskThrowsCheckedException;
+    private static Callable<String> taskToCompleteImmediately;
+    private static Callable<String> futureThatReturnsNull;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
     @BeforeClass
     public static void init() {
-        executorService = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(10));
 
-        taskToComplete = new Callable<Void>() {
+        taskToComplete = new Callable<String>() {
             @Override
-            public Void call() throws InterruptedException {
+            public String call() throws InterruptedException {
                 TimeUnit.MILLISECONDS.sleep(50);
-                return null;
+                return "complete";
             }
         };
 
-        taskToCompleteImmediately = new Callable<Void>() {
+        taskToCompleteImmediately = new Callable<String>() {
             @Override
-            public Void call() {
-                return null;
+            public String call() {
+                return "completeImmediatly";
             }
         };
 
-        futureThatTakesTooLong = new Callable<Void>() {
+        futureThatTakesTooLong = new Callable<String>() {
             @Override
-            public Void call() throws InterruptedException {
+            public String call() throws InterruptedException {
                 TimeUnit.SECONDS.sleep(5);
+                return "takesTooLong";
+            }
+        };
+
+        futureThatReturnsNull = new Callable<String>() {
+            @Override
+            public String call() throws InterruptedException {
                 return null;
             }
         };
 
-        taskThrowsACheckedException = new Callable<Void>() {
+        taskThrowsARuntimeException = new Callable<String>() {
             @Override
-            public Void call() throws Exception {
+            public String call() throws Exception {
+                throw new RuntimeException("unchecked");
+            }
+        };
+
+        taskThrowsCheckedException = new Callable<String>() {
+            @Override
+            public String call() throws Exception {
                 throw new Exception("checked");
             }
         };
+    }
 
-        taskThrowsAnUncheckedException = new Callable<Void>() {
-            @Override
-            public Void call() throws Exception {
-                throw new RuntimeException("Unchecked");
-            }
-        };
+    @Before
+    public void setup() {
+        executorService = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(10));
+    }
+
+    @After
+    public void tearDown() throws InterruptedException {
+        ExecutorServiceUtil.shutdownQuietly(executorService);
     }
 
     @Test
     public void timeoutTaskThatTakesTooLong() throws Exception {
 
-        List<ListenableFuture<Void>> futures = Lists.newArrayList();
+        List<ListenableFuture<String>> futures = Lists.newArrayList();
 
-        ListenableFuture<Void> future1 = executorService.submit(futureThatTakesTooLong);
-        ListenableFuture<Void> future2 = executorService.submit(taskToCompleteImmediately);
+        ListenableFuture<String> future1 = executorService.submit(futureThatTakesTooLong);
+        ListenableFuture<String> future2 = executorService.submit(taskToCompleteImmediately);
 
         futures.add(future1);
         futures.add(future2);
 
-        List<Void> results = MoreFutures.aggregate(futures, new Duration(TimeUnit.MILLISECONDS, 50));
+        List<String> results = MoreFutures.aggregate(futures, new Duration(TimeUnit.MILLISECONDS, 50));
+
+        assertThat(results.size(), is(1));
+        assertThat(future2.get(), equalTo(results.get(0)));
+        assertAllFutureAreComplete(futures);
+    }
+
+    @Test
+    public void removeNullValues() throws Exception {
+
+        List<ListenableFuture<String>> futures = Lists.newArrayList();
+
+        ListenableFuture<String> future1 = executorService.submit(futureThatReturnsNull);
+        ListenableFuture<String> future2 = executorService.submit(taskToCompleteImmediately);
+
+        futures.add(future1);
+        futures.add(future2);
+
+        List<String> results = MoreFutures.aggregate(futures, new Duration(TimeUnit.MILLISECONDS, 50));
 
         assertThat(results.size(), is(1));
         assertThat(future2.get(), equalTo(results.get(0)));
@@ -95,29 +125,30 @@ public class MoreFuturesTest {
     @Test
     public void taskThatThrowsRuntimeException() throws Exception {
 
-        List<ListenableFuture<Void>> futures = Lists.newArrayList();
+        List<ListenableFuture<String>> futures = Lists.newArrayList();
 
-        ListenableFuture<Void> future1 = executorService.submit(taskToComplete);
-        ListenableFuture<Void> future2 = executorService.submit(taskThrowsACheckedException);
-        ListenableFuture<Void> future3 = executorService.submit(taskToComplete);
-        ListenableFuture<Void> future4 = executorService.submit(taskToComplete);
+        ListenableFuture<String> future1 = executorService.submit(taskToComplete);
+        ListenableFuture<String> future2 = executorService.submit(taskThrowsARuntimeException);
+        ListenableFuture<String> future3 = executorService.submit(taskToComplete);
+        ListenableFuture<String> future4 = executorService.submit(taskToComplete);
 
         futures.add(future1);
         futures.add(future2);
         futures.add(future3);
         futures.add(future4);
 
-        List<Void> results = MoreFutures.aggregate(futures, new Duration(TimeUnit.MILLISECONDS, 200));
+        List<String> results = MoreFutures.aggregate(futures, new Duration(TimeUnit.MILLISECONDS, 200));
 
         assertAllFutureAreComplete(futures);
+        assertThat(results.size(), is(3));
     }
 
     @Test
     public void aggregateWithCancelledTask() throws MoreFuturesException {
 
-        List<ListenableFuture<Void>> futures = new ArrayList<ListenableFuture<Void>>();
+        List<ListenableFuture<String>> futures = new ArrayList<ListenableFuture<String>>();
 
-        ListenableFuture<Void> future = executorService.submit(futureThatTakesTooLong);
+        ListenableFuture<String> future = executorService.submit(futureThatTakesTooLong);
         future.cancel(true);
         futures.add(future);
 
@@ -144,19 +175,19 @@ public class MoreFuturesTest {
     @Test
     public void awaitOrThrowWhenCheckedExceptionIsThrown() throws MoreFuturesException {
         thrown.expect(MoreFuturesException.class);
-        ListenableFuture<Void> future = executorService.submit(taskThrowsACheckedException);
+        ListenableFuture<String> future = executorService.submit(taskThrowsCheckedException);
         MoreFutures.awaitOrThrow(future, MoreFuturesException.class);
     }
 
     @Test
     public void awaitOrThrowWhenUncheckedExceptionIsThrown() throws MoreFuturesException {
         thrown.expect(MoreFuturesException.class);
-        ListenableFuture<Void> future = executorService.submit(taskThrowsAnUncheckedException);
+        ListenableFuture<String> future = executorService.submit(taskThrowsARuntimeException);
         MoreFutures.awaitOrThrow(future, MoreFuturesException.class);
     }
 
     private void verifyCancelledAndExceptionThrown(Exception exception) throws InterruptedException, ExecutionException, TimeoutException {
-        ListenableFuture<Void> future = mock(ListenableFuture.class);
+        ListenableFuture<String> future = mock(ListenableFuture.class);
         when(future.get(anyLong(), any(TimeUnit.class))).thenThrow(exception);
         MoreFuturesException expected = null;
         try {
@@ -168,16 +199,9 @@ public class MoreFuturesTest {
         verify(future).cancel(true);
     }
 
-    private void assertAllFutureAreComplete(List<ListenableFuture<Void>> futures) {
+    private void assertAllFutureAreComplete(List<ListenableFuture<String>> futures) {
         for (ListenableFuture future : futures) {
             assertThat(future.isDone(), is(true));
-        }
-    }
-
-    @AfterClass
-    public static void tearDown() {
-        if (executorService != null && !executorService.isShutdown()) {
-            executorService.shutdown();
         }
     }
 
