6fae1d5e7fb335d49fbb123998e6b357390f5b28
nick spragg <nick.spragg@bbc.co.uk>
2014-03-27 10:12:46 +0000

    Added forwarding future

--- src/main/java/uk/co/bbc/iplayer/common/concurrency/ForwardingFutureTask.java
+++ src/main/java/uk/co/bbc/iplayer/common/concurrency/ForwardingFutureTask.java
@@ -1,7 +1,50 @@
 package uk.co.bbc.iplayer.common.concurrency;
 
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  * Created by spragn01 on 26/03/2014.
  */
-public class ForwardingFutureTask {
+public class ForwardingFutureTask<V> implements Future<V> {
+
+    private ListenableFuture<V> delegate;
+
+    public ForwardingFutureTask(ListenableFuture<V> delegate) {
+        checkNotNull(delegate, "expected future delegate, got null");
+        this.delegate = delegate;
+    }
+
+    @Override
+    public boolean cancel(boolean b) {
+        return delegate.cancel(b);
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return delegate.isCancelled();
+    }
+
+    @Override
+    public boolean isDone() {
+        return delegate.isDone();
+    }
+
+    @Override
+    public V get() throws InterruptedException, ExecutionException {
+        return delegate.get();
+    }
+
+    @Override
+    public V get(long l, TimeUnit timeUnit) throws InterruptedException, ExecutionException, TimeoutException {
+        return delegate.get(l, timeUnit);
+    }
+
+    public ListenableFuture<V> getDelegate() {
+        return delegate;
+    }
 }
--- src/main/java/uk/co/bbc/iplayer/common/concurrency/PipeableFutureTask.java
+++ src/main/java/uk/co/bbc/iplayer/common/concurrency/PipeableFutureTask.java
@@ -4,60 +4,43 @@ import com.google.common.util.concurrent.AsyncFunction;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import uk.co.bbc.iplayer.common.functions.ThrowableFunction;
+
 import javax.annotation.Nonnull;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Created by spragn01 on 25/03/2014.
  */
+public class PipeableFutureTask<V> extends ForwardingFutureTask<V> implements PipeableFuture<V> {
 
-// TODO: forwardingFutureTask
-public class PipeableFutureTask<V> implements PipeableFuture<V> {
-
-    private ListenableFuture<V> delegate;
-
+    /**
+     *
+     * @param delegate
+     * @param <V>
+     * @return
+     */
     public static <V> PipeableFuture<V> create(ListenableFuture<V> delegate) {
         return new PipeableFutureTask<V>(delegate);
     }
 
+    /**
+     *
+     * @param delegate
+     */
     private PipeableFutureTask(ListenableFuture<V> delegate) {
-        checkNotNull(delegate, "expected future delegate, got null");
-        this.delegate = delegate;
+        super(delegate);
     }
 
-    @Override
-    public boolean cancel(boolean b) {
-        return delegate.cancel(b);
-    }
-
-    @Override
-    public boolean isCancelled() {
-        return delegate.isCancelled();
-    }
-
-    @Override
-    public boolean isDone() {
-        return delegate.isDone();
-    }
-
-    @Override
-    public V get() throws InterruptedException, ExecutionException {
-        return delegate.get();
-    }
-
-    @Override
-    public V get(long l, TimeUnit timeUnit) throws InterruptedException, ExecutionException, TimeoutException {
-        return delegate.get(l, timeUnit);
-    }
-
+    /**
+     *
+     * @param throwableFunction
+     * @param <O>
+     * @return
+     */
     @Override
     public <O> PipeableFuture<O> to(@Nonnull final ThrowableFunction<V, O> throwableFunction) {
 
         return create(
-                Futures.transform(delegate, new AsyncFunction<V, O>() {
+                Futures.transform(getDelegate(), new AsyncFunction<V, O>() {
                     @Override
                     public ListenableFuture<O> apply(V input) throws Exception {
                         return Futures.immediateFuture(throwableFunction.apply(input));
