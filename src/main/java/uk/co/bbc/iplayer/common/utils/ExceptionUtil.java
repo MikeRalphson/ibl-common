934bfda20cf0d4b58e05c4f856360a7a9cea2dea
Tuncay Ok <tuncay.ok01.ext@bbc.co.uk>
2014-03-28 14:36:46 +0000

    code cleanup

--- src/main/java/uk/co/bbc/iplayer/common/utils/ExceptionUtil.java
+++ src/main/java/uk/co/bbc/iplayer/common/utils/ExceptionUtil.java
@@ -4,12 +4,12 @@ import org.apache.commons.lang.exception.ExceptionUtils;
 import org.slf4j.Logger;
 
 public class ExceptionUtil {
-    private static final String FAILED_TO_GET = "Thread failed: ";
+    private static final String THREAD_FAILED = "Thread failed: ";
     private static final String CAUSED_BY = "\nCaused by: ";
 
 
     public static void logExceptionSummary(Logger logger, String info, Throwable throwable) {
-        final StringBuilder sb = new StringBuilder(FAILED_TO_GET).append(info);
+        final StringBuilder sb = new StringBuilder(THREAD_FAILED).append(info);
 
         if (logger.isWarnEnabled()) {
             logger.warn(sb.toString(), throwable);
