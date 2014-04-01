88647c716acf6b5ffff51f0d4d8e6b60aafe0fe1
Tuncay Ok <tuncay.ok01.ext@bbc.co.uk>
2014-04-01 16:49:24 +0100

    added changes as per PR

--- src/main/java/uk/co/bbc/iplayer/common/utils/ExceptionUtil.java
+++ src/main/java/uk/co/bbc/iplayer/common/utils/ExceptionUtil.java
@@ -3,10 +3,13 @@ package uk.co.bbc.iplayer.common.utils;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.slf4j.Logger;
 
-public class ExceptionUtil {
+
+public final class ExceptionUtil {
     private static final String THREAD_FAILED = "Thread failed: ";
     private static final String CAUSED_BY = "\nCaused by: ";
 
+    private ExceptionUtil() {
+    }
 
     public static void logExceptionSummary(Logger logger, String info, Throwable throwable) {
         final StringBuilder sb = new StringBuilder(THREAD_FAILED).append(info);
