e4a7e726e09842ed72a04b6226c059b125031aac
nick spragg <nick.spragg@bbc.co.uk>
2014-04-01 20:49:27 +0100

    test interface...

---
+++ src/main/java/uk/co/bbc/iplayer/common/concurrency/MoreFutures3.java
@@ -0,0 +1,189 @@
+package uk.co.bbc.iplayer.common.concurrency;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import uk.co.bbc.iplayer.common.functions.ThrowableFunction;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.util.concurrent.MoreExecutors.listeningDecorator;
+import static uk.co.bbc.iplayer.common.concurrency.EvenMoreExecutors.*;
+
+public final class MoreFutures3 {
+
+    public static final Duration DEFAULT_DURATION = Duration.create();
+    public static final boolean INTERRUPT_TASK = true;
+
+    private MoreFutures3() {
+        throw new AssertionError();
+    }
+
+    /**
+     * @param token
+     * @param <T>
+     * @return
+     */
+    public static <T> Builder<T> chain(Class<T> token) {
+        return new Builder<T>();
+    }
+
+    /**
+     *
+     * Future Aggregation builder (DSL)
+     *
+     * @param <T>
+     */
+    public static class Builder<T> {
+
+        private Optional<ExecutorService> executorService = Optional.absent();
+        private ThrowableFunction<ListenableFuture<List<T>>, List<T>> aggregator;
+        private List<Callable<T>> tasks = Lists.newArrayList();
+        private Class<? extends Exception> onExceptionThrow = MoreFuturesException.class;
+
+        // Or use caller runs policy?
+        private static class DefaultExecutorServiceFactory {
+            public static final int MAX_THREAD_BOUND = (Runtime.getRuntime().availableProcessors() + 1);
+            public static ExecutorService ExecutorService =
+                    MoreExecutors.getExitingExecutorService((ThreadPoolExecutor) boundedNamedCachedExecutorService(MAX_THREAD_BOUND, "MoreFutureDefault"));
+        }
+
+        /**
+         * @param task
+         * @return
+         */
+        public Builder<T> add(Callable<T> task) {
+            tasks.add(checkNotNull(task));
+            return this;
+        }
+
+        /**
+         * @param collection
+         * @return
+         */
+        public Builder<T> addAll(Collection<Callable<T>> collection) {
+            tasks.addAll(checkNotNull(collection));
+            return this;
+        }
+
+        /**
+         * @param executorService
+         * @return
+         */
+        public Builder<T> usingExecutorService(ExecutorService executorService) {
+            this.executorService = Optional.of(checkNotNull(executorService));
+            return this;
+        }
+
+        /**
+         *
+         * @param toThrow
+         * @param <E>
+         * @return
+         */
+        public <E extends Exception> Builder<T> onExceptionThrow(Class<E> toThrow) {
+            this.onExceptionThrow = toThrow;
+            return this;
+        }
+
+        /**
+         * @param aggregator
+         * @return
+         * @throws Exception
+         */
+        public List<T> aggregate(ThrowableFunction<List<ListenableFuture<T>>, List<T>> aggregator) throws Exception {
+
+            // do we need a default executor (with automatic shutdown)?
+            if (!executorService.isPresent()) {
+                executorService = Optional.of(DefaultExecutorServiceFactory.ExecutorService);
+            }
+
+            ListeningExecutorService listeningExecutorService = listeningDecorator(executorService.get());
+
+            List<ListenableFuture<T>> futures = Lists.newArrayList();
+            for (Callable<T> task : tasks) {
+                futures.add(listeningExecutorService.submit(task));
+            }
+
+            return aggregator.apply(futures);
+        }
+
+        /**
+         * @return
+         * @throws Exception
+         */
+        public List<T> aggregate() throws Exception {
+            return aggregate(new Atomic<T>(onExceptionThrow));
+        }
+
+        /**
+         *
+         * @param function
+         * @param <S>
+         * @return
+         * @throws Exception
+         */
+        public <S> S aggregate(ThrowableFunction<List<T>, S> function) throws Exception {
+            return function.apply(aggregate());
+        }
+    }
+
+    /**
+     * @param <T>
+     */
+    public static class FilterSuccessful<T> implements ThrowableFunction<List<ListenableFuture<T>>, List<T>> {
+        @Override
+        public List<T> apply(List<ListenableFuture<T>> input) throws Exception {
+            ListenableFuture<List<T>> listenableFuture = Futures.successfulAsList(input);
+            return await(listenableFuture);
+        }
+    }
+
+    /**
+     * @param <T>
+     */
+    public static class Atomic<T> implements ThrowableFunction<List<ListenableFuture<T>>, List<T>> {
+
+        private Class<? extends Exception> toThrow = MoreFuturesException.class;
+
+        public Atomic(Class<? extends Exception> toThrow) {
+            this.toThrow = toThrow;
+        }
+
+        @Override
+        public List<T> apply(List<ListenableFuture<T>> input) throws Exception {
+            ListenableFuture<List<T>> listenableFuture = Futures.allAsList(input);
+            return await(listenableFuture, Duration.create(), toThrow);
+        }
+    }
+
+    public static <T> T await(ListenableFuture<? extends T> future) throws MoreFuturesException {
+        return await(future, DEFAULT_DURATION, MoreFuturesException.class);
+    }
+
+    public static <T, E extends Exception> T await(ListenableFuture<? extends T> future, Duration duration, Class<E> exception) throws E {
+        try {
+            return Futures.get(
+                    future,
+                    duration.getLength(),
+                    duration.getTimeUnit(),
+                    exception
+            );
+        } finally {
+            if (!future.isDone()) {
+                future.cancel(INTERRUPT_TASK);
+            }
+        }
+    }
+
+    public static <T> PipeableFuture<T> pipe(ListenableFuture<T> future) {
+        return PipeableFutureTask.create(future);
+    }
+}
---
+++ src/test/java/uk/co/bbc/iplayer/common/concurrency/MoreFutures3Test.java
@@ -0,0 +1,230 @@
+package uk.co.bbc.iplayer.common.concurrency;
+
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import uk.co.bbc.iplayer.common.functions.ThrowableFunction;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.*;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class MoreFutures3Test {
+
+    private ExecutorService executorService;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Before
+    public void setup() {
+        executorService = Executors.newFixedThreadPool(5);
+    }
+
+    @After
+    public void teardown() throws InterruptedException {
+        ExecutorServiceUtil.shutdownQuietly(executorService);
+    }
+
+    @Test
+    public void createFutureChainContainingUsingAdd() throws Exception {
+
+        List<String> aggregation = MoreFutures2
+                .chain(String.class)
+                .add(createTask("1"))
+                .add(createTask("2"))
+                .add(createTask("3"))
+                .usingExecutorService(executorService)
+                .aggregate();
+
+        assertThat(aggregation, hasItems("1", "2", "3"));
+    }
+
+    @Test
+    public void createFutureChainUsingDefaultExecutorService() throws Exception {
+
+        List<String> aggregation = MoreFutures2
+                .chain(String.class)
+                .add(createTask("1"))
+                .add(createTask("2"))
+                .add(createTask("3"))
+                .aggregate();
+
+        assertThat(aggregation, hasItems("1", "2", "3"));
+    }
+
+    @Test
+    public void createFutureChainUsingAddAll() throws Exception {
+
+        List<String> aggregation = MoreFutures2
+                .chain(String.class)
+                .addAll(newArrayList(createTask("1"), createTask("2"), createTask("3")))
+                .usingExecutorService(executorService)
+                .aggregate();
+
+        assertThat(aggregation, hasItems("1", "2", "3"));
+    }
+
+    @Test
+    public void customAggregation() throws Exception {
+        Integer length = MoreFutures2
+                .chain(String.class)
+                .addAll(newArrayList(createTask("I"), createTask("II"), createTask("III")))
+                .usingExecutorService(executorService)
+                .aggregate(new ThrowableFunction<List<String>, Integer>() {
+                    @Override
+                    public Integer apply(List<String> input) throws Exception {
+                        String max = Collections.max(input);
+                        return max.length();
+                    }
+                });
+
+        assertThat(length, is(3));
+    }
+
+    @Test
+    public void verifyExceptionConvertionWhenAnExceptionIsThrown() throws Exception {
+
+        expectedException.expect(TestFutureException.class);
+        expectedException.expectMessage("raised from task");
+
+        MoreFutures2
+                .chain(String.class)
+                .add(new Callable<String>() {
+                    @Override
+                    public String call() throws Exception {
+                        throw new Exception("raised from task");
+                    }
+                })
+                .onExceptionThrow(TestFutureException.class)
+                .usingExecutorService(executorService)
+                .aggregate();
+    }
+
+    @Test
+    public void awaitOnFutureWithinDefaultDuration() throws MoreFuturesException {
+        String expectedResult = "1";
+        int sleepTimeInSeconds = 1;
+        String value = MoreFutures2.await(
+                createListenableFuture(createTimedTask(expectedResult, sleepTimeInSeconds)));
+
+        assertThat(value, is(expectedResult));
+    }
+
+    @Test
+    public void awaitOnFutureExceedingDefaultDuration() throws MoreFuturesException {
+        String expectedResult = "returnValue";
+        int sleepTimeInMilliSeconds = 8000;
+
+        ListenableFuture<String> listenableFuture = createListenableFuture(createTimedTask(expectedResult, sleepTimeInMilliSeconds));
+
+        // using try as need to make further assertions
+        boolean thrown = false;
+        try {
+            MoreFutures2.await(listenableFuture);
+        } catch (MoreFuturesException e) {
+            thrown = true;
+        }
+
+        assertThat(thrown, is(true));
+        assertThat(listenableFuture.isDone(), is(true));
+    }
+
+    @Test
+    public void successfulGetFromPipeableFuture() throws ExecutionException, InterruptedException {
+
+        String sourceInput = "original";
+
+        ListenableFuture<String> sourceFuture = Futures.immediateFuture(sourceInput);
+
+        // disallow direct construction - only through moreFutures
+        PipeableFuture<String> pipeableFuture = PipeableFutureTask.create(sourceFuture);
+
+        PipeableFuture<Boolean> product = pipeableFuture
+                .to(new ThrowableFunction<String, Boolean>() {
+                    @Override
+                    public Boolean apply(String input) throws Exception {
+                        return input.startsWith("ori");
+                    }
+                });
+
+        Boolean match = product.get();
+
+        assertTrue(match);
+    }
+
+    @Test
+    public void createSimpleFuturePipeline() throws ExecutionException, InterruptedException {
+
+        // PipeableFutureTask to allow futures to be chain (using transform)
+        ListenableFuture<Boolean> future = Futures.immediateFuture(true);
+
+        PipeableFuture<String> pipeableFuture = MoreFutures2
+                .pipe(future)
+                .to(new ThrowableFunction<Boolean, String>() {
+                    @Override
+                    public String apply(Boolean input) throws Exception {
+                        return input.toString();
+                    }
+                });
+
+        String result = pipeableFuture.get();
+
+        assertThat(result, is("true"));
+    }
+
+    @Test
+    public void createFuturePipelineWithMultipleTransformations() throws ExecutionException, InterruptedException {
+
+        String input = "start";
+
+        ListenableFuture<String> future = Futures.immediateFuture(input);
+
+        PipeableFuture<Map<String, Integer>> pipeableFuture = MoreFutures2
+                .pipe(future)
+                    .to(new StringToASCII())
+                    .to(new ASCIIToString())
+                    .to(new CreateStringLengthMap());
+
+        Map<String, Integer> stringIntegerMap = pipeableFuture.get();
+
+        assertThat(stringIntegerMap.size(), is(1));
+        assertThat(stringIntegerMap.get(input), is(input.length()));
+    }
+
+    private Callable<String> createTask(final String returnStr) {
+        return new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return returnStr;
+            }
+        };
+    }
+
+    private Callable<String> createTimedTask(final String returnStr, final int seconds) {
+        return new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                TimeUnit.MILLISECONDS.sleep(seconds);
+                return createTask(returnStr).call();
+            }
+        };
+    }
+
+    private <T> ListenableFuture<T> createListenableFuture(Callable<T> task) {
+        return MoreExecutors.listeningDecorator(executorService).submit(task);
+    }
+
+}
