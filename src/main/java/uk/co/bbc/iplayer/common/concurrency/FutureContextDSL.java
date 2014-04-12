670b7983761ae1633057bff2e2f0226a5b4e3141
nick spragg <nick.spragg@bbc.co.uk>
2014-04-12 10:04:38 +0100

    test

--- src/main/java/uk/co/bbc/iplayer/common/concurrency/FutureContextDSL.java
+++ src/main/java/uk/co/bbc/iplayer/common/concurrency/FutureContextDSL.java
@@ -1,7 +1,48 @@
 package uk.co.bbc.iplayer.common.concurrency;
 
-/**
- * Created by spragn01 on 10/04/2014.
- */
+import com.google.common.util.concurrent.ListenableFuture;
+import uk.co.bbc.iplayer.common.functions.ThrowableFunction;
+
 public class FutureContextDSL {
+
+    public static FutureContextDSL createFuturePipeline() {
+        return null;
+    }
+
+    public FutureContextDSL add(ListenableFuture future) {
+        return null;
+    }
+
+    public FutureContextDSL onException() {
+        return null;
+    }
+
+    public ExecutingFutureContextDSL execute() {
+        return null;
+    }
+
+    public FutureContextDSL foreach() {
+        return null;
+    }
+
+    public PipeableFuture build() {
+        return null;
+    }
+
+    class ExecutingFutureContextDSL extends FutureContextDSL {
+
+        private FutureContextDSL futureContextDSL;
+
+        public ExecutingFutureContextDSL(FutureContextDSL futureContextDSL) {
+            this.futureContextDSL = futureContextDSL;
+        }
+
+        public ExecutingFutureContextDSL execute() {
+            return null;
+        }
+
+        public ExecutingFutureContextDSL foreach(ThrowableFunction f) {
+            return null;
+        }
+    }
 }
