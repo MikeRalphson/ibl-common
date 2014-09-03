3b42da5d938a81b1b928b05eb6d1445d6be7688e
Robin Murphy <robin.murphy@bbc.co.uk>
2014-09-03 10:35:16 +0100

    Add wrapper classes for serializable sets and lists

---
+++ src/main/java/uk/co/bbc/iplayer/common/definition/SerializableSet.java
@@ -0,0 +1,19 @@
+package uk.co.bbc.iplayer.common.definition;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
+
+public class SerializableSet<K extends Serializable> implements Serializable {
+
+    private final Set<K> set;
+
+    public SerializableSet(Set<K> set) {
+        this.set = set;
+    }
+
+    public Set<K> getList() {
+        return set;
+    }
+
+}
